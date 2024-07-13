189cf275326215995c2cad2b7f542eb17994926f
Nayemulislam24 <nayemtp@gmail.com>
2024-07-13 23:36:26 +0600

    laravel session practice

--- project-one/app/Http/Controllers/sessionController.php
+++ project-one/app/Http/Controllers/sessionController.php
@@ -6,5 +6,29 @@ use Illuminate\Http\Request;
 
 class sessionController extends Controller
 {
-    //
+    function sessionPut(Request $request): bool
+    {
+        $email =  $request->email;
+        $request->session()->put('userEmail', $email);
+        return true;
+    }
+
+    function sessionPull(Request $request): string
+    {
+
+        return $request->session()->pull('userEmail');
+    }
+
+    function sessionForget(Request $request): bool
+    {
+
+        $request->session()->forget('keys', 'userEmail');
+        return true;
+    }
+
+    function sessionFlash(Request $request): bool
+    {
+        $request->session()->flush();
+        return true;
+    }
 }
